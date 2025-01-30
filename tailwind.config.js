b0c183830fbd7ed4d66cd75bf7ee8791a8f6c02a
Benjamin Le <benle@udel.edu>
2025-01-30 00:25:53 -0500

    Better text scalability with different window sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,19 +15,19 @@ module.exports = {
             transform: 'translateY(0%)',
           },
           '20%, 36%': {
-            transform: 'translateY(-16.66%)',
+            transform: 'translateY(-16.66%)', /**-16.66 */
           },
           '40%, 56%': {
-            transform: 'translateY(-33.33%)',
+            transform: 'translateY(-33.33%)', /**-33.33 */
           },
           '60%, 76%': {
-            transform: 'translateY(-50%)',
+            transform: 'translateY(-50%)', /**-50 */
           },
           '80%, 96%': {
-            transform: 'translateY(-66.66%)',
+            transform: 'translateY(-66.66%)', /** -66.66 */
           },
           '100%': {
-              transform: 'translateY(-83.33%)',
+              transform: 'translateY(-83.33%)', /**83.33 */
           },
         }
       }
