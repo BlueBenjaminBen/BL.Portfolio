a9bd5d43a9934cfe335c297b1a193ecd7012d263
Benjamin Le <benle@udel.edu>
2025-02-03 21:15:53 -0500

    reformatted folder structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./build/*.html"],
+  content: ["./src/pages/HTML/*.html"],
   theme: {
     extend: {
       fontFamily:{
