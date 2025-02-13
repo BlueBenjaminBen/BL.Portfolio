a7ed016e4dd834293525260ccd6f6dda7cb28291
Benjamin Le <benle@udel.edu>
2025-02-12 20:10:58 -0500

    added js folder to build

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./build/HTML/**/*.html"],
+  content: ["./build/**/*.{html,js}"],
   theme: {
     extend: {
       fontFamily:{
