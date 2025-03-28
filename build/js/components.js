047852a6e529fb2d0b4d10fc0a83a54e987c8b3c
Benjamin Le <benle@udel.edu>
2025-03-28 16:16:44 -0400

    including repo name in basePath for components.js

--- build/js/components.js
+++ build/js/components.js
@@ -11,7 +11,8 @@ async function loadComponent(id, file) {
 document.addEventListener("DOMContentLoaded", () => {
     // Check if the current page is the root or inside the HTML folder
 // This will set the basePath based on whether we're at the root or inside a folder
-const basePath = window.location.pathname.includes("/HTML/") ? "../HTML" : "build/HTML";
+let basePath = "/BL.Portfolio/";
+ basePath = window.location.pathname.includes("/HTML/") ? "../HTML" : "build/HTML";
 
     loadComponent("navbar", basePath + "/components/navbar.html");
     loadComponent("footer", basePath + "/components/footer.html");
