1a4cf749543d562c1636759dd5a65e0938f4de5a
Benjamin Le <benle@udel.edu>
2025-08-27 20:37:08 -0400

    Made landing page adjust size dynamically

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,27 +7,27 @@ module.exports = {
         'monoton': ['Monoton', 'sans-serif'],
       },
       animation:{
-        'text-slide': 'text-slide 9.5s cubic-bezier(0.83, 0, 0.17, 1) infinite',
+        'text-slide-em': 'text-slide-em 9.5s cubic-bezier(0.83, 0, 0.17, 1) infinite',
       },
       keyframes: {
-        'text-slide': {
+        'text-slide-em': {
           '0%, 16%': {
-            transform: 'translateY(0%)',
+            transform: 'translateY(0em)',
           },
           '20%, 36%': {
-            transform: 'translateY(-16.66%)', /**-16.66 */
+            transform: 'translateY(-1.1em)', /**-16.66 */
           },
           '40%, 56%': {
-            transform: 'translateY(-33.33%)', /**-33.33 */
+            transform: 'translateY(-2.1em)', /**-33.33 */
           },
           '60%, 76%': {
-            transform: 'translateY(-50%)', /**-50 */
+            transform: 'translateY(-3.1em)', /**-50 */
           },
           '80%, 96%': {
-            transform: 'translateY(-66.66%)', /** -66.66 */
+            transform: 'translateY(-4.1em)', /** -66.66 */
           },
           '100%': {
-              transform: 'translateY(-83.33%)', /**83.33 */
+              transform: 'translateY(-5.1em)', /**83.33 */
           },
         }
       }
